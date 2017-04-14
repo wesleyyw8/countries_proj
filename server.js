5cf1b52d43e132374abf50fb54a692943f393a21
WESLEY REBELO <wesleyrebelo@WESLEYs-MacBook-Pro.local>
2017-04-14 18:04:45 +0100

    bascic structure done

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/ui')); 
+
+var portNumber = 1234;
+var server = app.listen(process.env.PORT || portNumber,function(){
+  console.log("We have started our server on port! "+portNumber);
+});
+app.get('/endpoints/countryInfoJSON', function(request,response){
+  response.sendFile(__dirname + '/countryInfoJSON.json');
+});
+app.get('*', function(req, res) {
+  res.sendfile('./ui/views/index.html');
+});
